2fc8f868163476150fa3ea7910287bfb3dba9d28
sugarliu <450755513@qq.com>
2017-08-17 19:39:14 +0800

    finish practice-1 about class and extends

--- main/practice-1/person.js
+++ main/practice-1/person.js
@@ -1,2 +1,26 @@
 // Write your code here
-module.exports = class Person {}
+//traditional method
+/**
+function Person(name,age) {
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.introduce = function () {
+    return "My name is " + this.name + ". I am " + this.age + " yesrs old.";
+}
+
+var p = new person("Tom",20);
+console.log(p.introduce());
+module.exports = Person;
+ **/
+//ES6 class
+class Person{
+    constructor(name, age){
+        Person.prototype.name = name;
+        Person.prototype.age = age;
+    }
+    introduce(){
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+}
+module.exports = Person;
--- main/practice-1/student.js
+++ main/practice-1/student.js
@@ -1,3 +1,17 @@
 const Person = require('./person');
+class Student extends Person{
+    constructor(name,age,number){
+        super(name,age);
+        this.class = number;
+    }
 
-module.exports = class Student {}
+    introduce(){
+        return "My name is " +super.name+ ". I am " +super.age+ " years old. I am a Student. I am at Class "+ this.class+".";
+    }
+}
+
+// var s = new Student("Tom", 21, 2);
+// console.log(s.introduce());
+
+// module.exports = class Student {}
+module.exports = Student;
